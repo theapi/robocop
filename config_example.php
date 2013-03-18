43a71a0527173b7198bf394c561aa1e9811188e5
theapi <petermclarke@gmail.com>
2013-03-18 19:22:57 +0000

    Example config file

---
+++ config_example.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Configuration 
+ */
+
+$conf = array(
+
+  'save_dir' => '/home/cctv',
+  'email' = array(
+    'from' => 'my.email@gmail.com',
+    'to' = > 'my.email@gmail.com',
+    'send_interval' => '60',
+  ),
+  'smtp' = array(
+    'username' => 'my.emai.@gmail.com',
+    'password' => 'secret',
+  ),
+  
+)
+
