a04e39de2707bc4836a541241752643134ed5d65
Peter Clarke <petermclarke@gmail.com>
2013-03-31 13:40:45 +0100

    Cache the container with PhpDumper

---
+++ src/Theapi/Robocop/Console/Command/ClearCacheCommand.php
@@ -0,0 +1,40 @@
+<?php
+namespace Theapi\Robocop\Console\Command;
+
+
+use Theapi\Robocop\Console\RobocopApplication;
+
+use Symfony\Component\Console\Command\Command,
+    Symfony\Component\Console\Input,
+    Symfony\Component\Console\Input\InputOption,
+    Symfony\Component\Console\Input\InputArgument,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Output\OutputInterface;
+
+class ClearCacheCommand extends Command
+{
+
+    public function __construct()
+    {
+        parent::__construct('email:send');
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('cache:clear')
+            ->setDescription('Clears the Robocop caches')
+            ->setHelp('Clears the <info>config cache</info> so new changes to config.xml will be used.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+      $file = RobocopApplication::getContainerCacheFile();
+      if (file_exists($file)) {
+        unlink($file);
+      }
+      $output->writeln(sprintf('Cache cleared'));
+    }
+
+}
